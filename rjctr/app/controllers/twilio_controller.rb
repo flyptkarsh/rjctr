b2ce33344e62ad3b25cdb8d94e29163696bd08b5
flyptkarsh <ptkarsh@gmail.com>
2014-07-11 15:30:57 -0400

    enviromental variables

--- rjctr/app/controllers/twilio_controller.rb
+++ rjctr/app/controllers/twilio_controller.rb
@@ -3,14 +3,13 @@ class TwilioController < ApplicationController
 	
 	def log
 		# Get your Account Sid and Auth Token from twilio.com/user/account
-		account_sid = 'AC3c076ac9ad764ed233de4624370a8b97'
-		auth_token = '2b9c570ca4b1fa9e9a68d29f39a08b53'
+		account_sid = ENV["ACCOUNT_SID"] 
+		auth_token = 	ENV["TOKEN"]
 		@client = Twilio::REST::Client.new account_sid, auth_token
 	end 
 
 	def search 
-		account_sid = 'AC3c076ac9ad764ed233de4624370a8b97'
-		auth_token = '2b9c570ca4b1fa9e9a68d29f39a08b53'
+
 		@client = Twilio::REST::Client.new account_sid, auth_token
 	end 
 
@@ -94,11 +93,9 @@ class TwilioController < ApplicationController
 		 	message = "OMG that is totally something Justin Bieber would say!"
 		 end 
 		#this interfaces with the API
-
 		twiml = Twilio::TwiML::Response.new do |r|
 			r.Message "#{message}"
 		end
-
 		render text: twiml.text
 	end
 
